d950e730a753ad8be227f8a0ce86205ce65f00f2
joseguzman1337 <joseguzman1337@users.noreply.github.com>
2026-03-18 16:49:36 -0500

    chore(sync): incorporate AI agents work and sync

---
+++ metadata.rb
@@ -0,0 +1,20 @@
+name              'passenger_apache2'
+maintainer        'Chef Software, Inc.'
+maintainer_email  'cookbooks@chef.io'
+license           'Apache 2.0'
+description       'Installs passenger for Apache2'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '2.3.0'
+
+recipe 'passenger_apache2::default', 'Installs Passenger as an Apache module'
+recipe 'passenger_apache2::mod_rails', 'Enables Apache module configuration for passenger module'
+
+depends 'apache2', '>= 2.0'
+depends 'build-essential'
+
+%w( fedora redhat centos scientific amazon oracle ubuntu debian arch suse ).each do |os|
+  supports os
+end
+
+source_url 'https://github.com/chef-cookbooks/passenger_apache2' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/passenger_apache2/issues' if respond_to?(:issues_url)
